eaee021d5ecef2cfc584c07cf1d88c56ce48ac6a
migueling2015 <mprada.ing@gmail.com>
2020-04-26 18:17:29 -0500

    Cuphead

---
+++ scripts/functional.js
@@ -0,0 +1,32 @@
+var array_big_characters=[
+    [,,],
+    [,,],
+    [,,],
+    [,,],
+    [,,],
+    [,,],
+    [,,],
+    [,,],
+    [,,],
+    [,,],
+    [,,],
+    '<img src="assets/char_le_grande_big.jpg" alt="" >',
+    '<img src="assets/char_the_root_pack_big.jpg" alt="" >',
+    '<img src="assets/char_wernwer_werman_big.jpg" alt="" >',
+    [,,],
+]
+
+function on(char_index) { 
+  document.getElementById("overlay").style.display = "block";
+  let image=document.getElementById("overlay-img");
+  image.innerHTML=array_big_characters[char_index];
+
+//   document.getElementById("overlay-img").style.top= window.screen.height;
+//   document.getElementById("overlay-img").style.left="200px";
+//   document.getElementById("overlay-text").innerHTML=array_big_characters[char_index][1]+array_big_characters[char_index][2];
+
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
